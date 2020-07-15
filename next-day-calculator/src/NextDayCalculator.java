544f22e7a1dbefa294a2a4dd79eec29204d733ca
unknown <thuyle8824@gmail.com>
2020-07-15 11:18:54 +0700

    add

---
+++ next-day-calculator/src/NextDayCalculator.java
@@ -0,0 +1,72 @@
+import java.time.Year;
+
+public class NextDayCalculator {
+    public static String nextDay(int day, int month, int year) {
+        String result="";
+        boolean leapYear=NextDayCalculator.isLeapYear(year);
+        if (day<1||month<1||year<1||month>12) {
+            throw new UnsupportedOperationException();
+        }
+        switch (month) {
+            case 1:
+            case 3:
+            case 5:
+            case 7:
+            case 8:
+            case 10:
+                if (day>31)
+                    throw new UnsupportedOperationException();
+                else if (day==31)
+                    result= 1+"-"+(month+1)+"-"+year;
+                else result= day+1 +"-"+month+"-"+year;
+                break;
+            case 4:
+            case 6:
+            case 9:
+            case 11:
+                if (day>30)
+                    throw new UnsupportedOperationException();
+                else if (day==30)
+                    result= 1+"-"+(month+1)+"-"+year;
+                else result= day+1 +"-"+month+"-"+year;
+                break;
+            case 12:
+                if (day>31)
+                    throw new UnsupportedOperationException();
+                else if (day==31)
+                    result= 1+"-"+1+"-"+(year+1);
+                else result= day+1 +"-"+month+"-"+year;
+                break;
+            case 2:
+                if (leapYear) {
+                    if (day>29)
+                        throw new UnsupportedOperationException();
+                    else if (day==29)
+                        result=1 +"-"+3+"-"+year;
+                    else result=day+1 +"-"+month+"-"+year;
+                } else {
+                    if (day>28)
+                        throw new UnsupportedOperationException();
+                    else if (day==28)
+                        result=1 +"-"+3+"-"+year;
+                    else result=day+1 +"-"+month+"-"+year;
+                }
+                break;
+        }
+        return result;
+    }
+    public static boolean isLeapYear(int year) {
+        boolean isDivisibleBy4 = year % 4 == 0;
+        if (isDivisibleBy4) {
+            boolean isDivisibleBy100 = year % 100 == 0;
+            if (isDivisibleBy100) {
+                boolean isDivisibleBy400 = year % 400 == 0;
+                if (isDivisibleBy400)
+                    return true;
+            } else {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ next-day-calculator/test/NextDayCalculatorTest.java
@@ -0,0 +1,75 @@
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class NextDayCalculatorTest {
+    @Test
+    @DisplayName("")
+    public void nextDayOf112018() {
+        int day=1;
+        int month=1;
+        int year=2018;
+        String expected="2-1-2018";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("")
+    public void nextDayOf3112018() {
+        int day=31;
+        int month=1;
+        int year=2018;
+        String expected="1-2-2018";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("")
+    public void nextDayOf3042018() {
+        int day=30;
+        int month=4;
+        int year=2018;
+        String expected="1-5-2018";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("")
+    public void nextDayOf2822018() {
+        int day=28;
+        int month=2;
+        int year=2018;
+        String expected="1-3-2018";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("")
+    public void nextDayOf2922020() {
+        int day=29;
+        int month=2;
+        int year=2020;
+        String expected="1-3-2020";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+    @Test
+    @DisplayName("")
+    public void nextDayOf31122018() {
+        int day=31;
+        int month=12;
+        int year=2018;
+        String expected="1-1-2019";
+        String result=NextDayCalculator.nextDay(day,month,year);
+        assertEquals(expected,result);
+    }
+
+}
